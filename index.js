1fb6767804faaa89d4c4350810207ae799eb9e58
USER <+>
2023-06-16 18:57:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+module.exports = cats;
+
+
+// destructivelyPrependCat(name)
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  // destructivelyRemoveLastCat()
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  // destructivelyRemoveFirstCat()
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  // appendCat(name)
+  function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+  }
+  
+  // prependCat(name)
+  function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+  }
+  
+  // removeLastCat()
+  function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+  }
+  
+  // removeFirstCat()
+  function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+  }
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+  function appendCat(name) {
+    return [...cats, name];
+  }
+  
